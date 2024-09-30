074eff5bd4e65c5ed5fdb3b8603493ce1034b1eb
Matheus Sousa <matheus.sousa@omnisaude.app>
2024-09-30 02:42:51 -0300

    feat: make docker work

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true, // Should be disabled in production.
+    }),
+  ],  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
